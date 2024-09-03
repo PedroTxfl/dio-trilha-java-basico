19da26ae83a2a8b36f3f0f2b4bf00f0e3a4d0d62
txfl <111077008+PedroTxfl@users.noreply.github.com>
2024-09-03 16:55:09 -0300

    Inicio estruturação código

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(new Locale("pt", "BR"));
+        
+        System.out.println("Por favor, digite seu nome:");
+        String nomeCliente = scanner.nextLine();
+        
+        System.out.println("Digite o número da sua agência:");
+        
+        
+        System.out.println("Digite o número da sua conta:");
+        int numero = scanner.nextInt();
+        
+        System.out.println("Digite seu saldo:");
+        //TODO: Conhecer e importar a classe scanner
+        //exibir as mensagens ao usuário
+        //Obter pela classe scanner os valores digitados no terminal
+        //exibir a mensagem da conta criada
     }
 }
